40f2a9ecc96faf05dea0a871ac77b4c9c7e461b5
David <davidcmoran20@gmail.com>
2024-10-09 13:20:32 +0200

    update services and cli

--- githubActivityCli.js
+++ githubActivityCli.js
@@ -1,4 +1,7 @@
 import readline from "readline";
+import GithubService from "./githubService.js";
+
+const githubService = new GithubService();
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -6,11 +9,14 @@ const rl = readline.createInterface({
   prompt: "github-activity ",
 });
 
-console.log('Task Tracker. Type "help" for available commands.');
+console.log("Github activity");
 rl.prompt();
 
 rl.on("line", (line) => {
-  const [command, ...args] = line.trim().split(" ");
+  const [username] = line.trim().split(" ");
+
+  githubService.getUserGithubEvents(username);
+
   rl.prompt();
 }).on("close", () => {
   console.log("Exit.");
--- githubService.js
+++ githubService.js
@@ -1,22 +1,64 @@
+import https from "https";
+
 class GithubService {
   constructor() {}
 
-  fetchData(username) {
+  getUserGithubEvents(username) {
     const apiUrl = `https://api.github.com/users/${username}/events`;
 
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network error: " + response.status);
-        }
-        return response.json(); // Parsear la respuesta JSON
-      })
-      .then((data) => {
-        console.log(data); // Aquí puedes manejar los datos recibidos
+    const options = {
+      headers: {
+        "User-Agent": "Node.js",
+      },
+    };
+
+    this.fetchData(apiUrl, options)
+      .then((events) => {
+        events.forEach((event) => {
+          switch (event.type) {
+            case "PushEvent":
+              break;
+            case "DeleteEvent":
+              break;
+            case "CreateEvent":
+              break;
+
+            default:
+              break;
+          }
+          console.log(
+            `ID: ${task.id}, Description: ${task.description}, Status: ${task.status}, CreatedAt: ${task.createdAt}, UpdatedAt: ${task.updatedAt}`
+          );
+        });
+        console.log(data);
       })
       .catch((error) => {
-        console.error("There was a problem with the API call:", error);
+        console.error(error);
       });
+  }
+
+  fetchData(apiUrl, options) {
+    return new Promise((resolve, reject) => {
+      https
+        .get(apiUrl, options, (response) => {
+          let data = "";
+
+          response.on("data", (chunk) => {
+            data += chunk;
+          });
+
+          response.on("end", () => {
+            if (response.statusCode === 200) {
+              resolve(JSON.parse(data));
+            } else {
+              reject(`Error: ${response.statusCode}`);
+            }
+          });
+        })
+        .on("error", (error) => {
+          reject(`Request failed: ${error.message}`);
+        });
+    });
   }
 }
 
